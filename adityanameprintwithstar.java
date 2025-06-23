31756ef0195faed0e7375dee6d7c6aae3ac7bee5
AdityaGore <adityagore617@gmail.com>
2025-06-23 10:30:26 -0700

    Add files via upload

---
+++ adityanameprintwithstar.java
@@ -0,0 +1,7 @@
+class Nameprintwithstar{
+     public static void main(String[] args){
+            System.out.println("**********");
+            System.out.println("*Aditya gore*");
+            System.out.println("**********");
+     }  
+}
