b0d3abae769b94c1fe11d96300c4283b7186faab
AdityaGore <adityagore617@gmail.com>
2025-06-23 11:12:54 -0700

    Add files via upload

---
+++ adityapersonalinfo.java
@@ -0,0 +1,9 @@
+class adityapersonalinfo{
+     public static void main(String[] args){
+          String name="Aditya Gore";
+          int age=20;
+          String course="core java";
+          float fee=3500;
+          char gender='m';
+          }
+}
