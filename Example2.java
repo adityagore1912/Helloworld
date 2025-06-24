c8ea550358af02032968e0d1a8ce3b4cfca95a97
AdityaGore <adityagore617@gmail.com>
2025-06-24 10:11:00 -0700

    Add files via upload

---
+++ Example2.java
@@ -0,0 +1,18 @@
+class example5{
+  int i1=20;
+  float f1=1.2f;
+  String s1="aditya";
+  boolean b1=true;
+  char[] c1={'A','B'};
+ }
+class test06_class1{
+public static void main(String[] args){
+example5 e2=new example5();
+System.out.println(e2.i1);
+System.out.println(e2.f1);
+System.out.println(e2.s1);
+System.out.println(e2.b1);
+System.out.println(e2.c1[0]);
+System.out.println(e2.c1[1]);
+}
+}
